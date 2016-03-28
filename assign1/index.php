b68f32198ccc70b86618e135b9af37638d3cd886
Duan Hao <haoduan@outlook.com>
2016-03-28 20:30:56 +1300

    update @ 8:30PM 28/03/2016

---
+++ assign1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Status Posting System</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="amazeui.js"></script>
+    </head>
+    <body>
+	<div class="am-g am-nav-bg-color-primary">
+	    <div class="am-container am-cf">
+		<h1 class="am-fl am-icon-paper-plane am-header-font-color" style="margin-top: 12px;"> Status Posting System</h1>
+		<ul class="am-fr am-nav am-nav-pills">
+		    <li class="am-active"><a href="#">Home</a></li>
+		    <li><a href="#">Post New</a></li>
+		    <li><a href="#">Search</a></li>
+		    <li><a href="#">About</a></li>
+		</ul>
+	    </div>
+	</div>
+    </body>
+</html>
+    
