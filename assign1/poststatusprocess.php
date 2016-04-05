2c38e2e840eea312e088403952d47fa084fb89bf
Duan Hao <haoduan@outlook.com>
2016-04-05 20:20:21 +1200

    update @ 8:20PM 05/04/2016

--- assign1/poststatusprocess.php
+++ assign1/poststatusprocess.php
@@ -5,11 +5,10 @@ require_once 'Post.class.php';
 DB::init();
 
 //echo '<script type="text/javascript">alert("'.DB::DB_HOST.'");</script>';
-
 $notice = Post::validate_status_code($_POST['status_code']);
 
 if ( ! empty( $notice ) ) {
     setcookie('notice_msg', $notice);
-    header("Location: poststatusform.php");
+    header("Location: http://".($_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']))."/poststatusform.php");
     exit();
 }
